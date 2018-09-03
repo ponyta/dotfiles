74dd491cbbd37081b3a3ac3589a74ed36b458cfb
Chun Li <chunli.developer@gmail.com>
2018-09-03 16:36:07 -0400

    Small cleanup.

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DOTFILES=$(pwd)
-ln -si $DOTFILES/vimrc ~/.vimrc
+
+ln -s $DOTFILES/config ~/.config
 ln -si $DOTFILES/agignore ~/.agignore
 ln -si $DOTFILES/gitconfig ~/.gitconfig
 ln -si $DOTFILES/profile ~/.profile
-ln -si $DOTFILES/vimrc.bundles ~/.vimrc.bundles
 ln -si $DOTFILES/tmux.conf ~/.tmux.conf
-ln -si $DOTFILES/config ~/.config
+ln -si $DOTFILES/vimrc ~/.vimrc
+ln -si $DOTFILES/vimrc.bundles ~/.vimrc.bundles
