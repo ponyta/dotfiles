cb82a724ac8d27e34b35b4db48e9bd87e754261b
Chun Li <chunli.developer@gmail.com>
2018-09-03 16:30:01 -0400

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DOTFILES=$(pwd)
+ln -si $DOTFILES/vimrc ~/.vimrc
+ln -si $DOTFILES/agignore ~/.agignore
+ln -si $DOTFILES/gitconfig ~/.gitconfig
+ln -si $DOTFILES/profile ~/.profile
+ln -si $DOTFILES/vimrc.bundles ~/.vimrc.bundles
+ln -si $DOTFILES/tmux.conf ~/.tmux.conf
+ln -si $DOTFILES/config ~/.config
---
+++ profile
@@ -0,0 +1,3 @@
+[ -n "$PROFILE" ] && return || readonly PROFILE=1
+
+export PATH="$HOME/.cargo/bin:$PATH"
