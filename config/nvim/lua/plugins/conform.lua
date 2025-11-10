9ecf270cec8d5a164f21cfb28f966734b06dfec2
Chun Li <chunli.developer@gmail.com>
2025-11-10 09:30:39 -0800

    Add markdown conform configuration to softwrap at 120 chars.

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -2,8 +2,21 @@ return {
   {
     'stevearc/conform.nvim',
     opts = {
+      formatters = {
+        prettier_markdown = {
+          command = 'prettier',
+          args = {
+            '--parser=markdown',
+            '--prose-wrap',
+            'always',
+            '--print-width',
+            '120',
+          },
+        },
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
+        markdown = { 'prettier_markdown' },
         javascript = { 'prettier' },
       },
       format_on_save = {
